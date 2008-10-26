97e19afc59b894957230662872f3bb02b7d52b14
Jakub Narebski <jnareb@gmail.com>
2008-10-26 17:20:18 +0100

    SoftSnow XChat Filter 1.2.2 (from backup file)
    Cleanup: use /usr/bin/perl in shebang (#!) line, put script version in
    variable, just like script name.
    
    Add /FILTER command, with ON, OFF, STATUS, HELP, SERVER, ALL
    subcommands.  Those allow turning filter on and off, limiting
    filtering to current IRC server or filtering all output, checking
    status of filter (if it is turned on, if it is limited to single
    server), and getting help.  Filter is now turned OFF by default.
    
    NOTE: '/filter help' had bug showing that '/filter' toggles filter on
    and off, while in 1.2.2 one use '/filter on' and '/filter off' for
    that.
    
    Beginnings of putting regular expressions defining filter in @filter
    array.  All regular expressions are yet embedded literally (hardcoded)
    in isFiltered() subroutine.  Added /~&~&~/ as filter rule.
    
    Signed-off-by: Jakub Narebski <jnareb@gmail.com>

--- SoftSnow_filter.pl
+++ SoftSnow_filter.pl
@@ -1,19 +1,77 @@
-#!perl -w
+#!/usr/bin/perl -w
 
 use strict;
 
-my $scriptName = "SoftSnow XChat Filter";
+my $scriptName    = "SoftSnow XChat Filter";
+my $scriptVersion = "1.2.2";
 
-IRC::register($scriptName, "1.2", "", "");
+IRC::register($scriptName, $scriptVersion, "", "");
 
 #IRC::add_command_handler("amsg", "amsg_command_handler");
+IRC::add_command_handler("filter",  "filter_command_handler");
 IRC::add_message_handler("PRIVMSG", "privmsg_handler");
-
-IRC::print("Loading $scriptName\n");
 
 my $code_bold   = chr 2;
 my $code_under  = chr 31;
 my $code_colour = chr 3;
+
+my $command_list = 'ON|OFF|STATUS|HELP|SERVER|ALL';
+
+IRC::print("Loading $code_bold$scriptName $scriptVersion$code_bold\n"
+	  ." Commands: ${code_bold}/FILTER ${command_list}${code_bold}\n");
+
+my $filter_turned_on = 0; # was default turned ON
+my $limit_to_server  = 0; # don't limit to server
+
+if ($filter_turned_on) {
+  IRC::print("Filter turned ${code_bold}ON${code_bold}\n");
+} else {
+  IRC::print("Filter turned ${code_bold}OFF${code_bold}\n");
+}
+if ($limit_to_server) {
+  IRC::print("Filtering limited to server $limit_to_server\n")
+}
+
+my @filter = 
+  (
+   /\@/, 
+   /^\s*\!/, 
+   /slot\(s\)/, 
+   /~&~&~/,
+  
+  #xdcc
+  /^\#\d+/, 
+  
+  #fserves
+  /fserve.*trigger/i, 
+  /trigger.*\!/i, 
+  /trigger.*\/ctcp/i, 
+  /type\:\s*\!/i, 
+  /file server online/i, 
+  
+  #ftps
+  /ftp.*l\/p/i, 
+  
+  #CTCPs
+  /SLOTS/, 
+  /MP3 /, 
+  
+  #messages for when a file is received/failed to receive
+  /DEFINITELY had the right stuff to get/i, 
+  /has just received/i, 
+  /I have just received/i, 
+  
+  #mp3 play messages
+  /is listening to/, 
+  /\]\-MP3INFO\-\[/, 
+  
+  #spammy scripts
+  /\]\-SpR\-\[/, 
+  /We are BORG/, 
+  
+  #general messages
+  /brave soldier in the war/
+);
 
 sub isFiltered {
   my $text = $_[0];
@@ -32,9 +90,14 @@ sub isFiltered {
 #    IRC::print("isFiltered : text=". $text. "\n");
 #  }
 
+  foreach ($regexp in @filter) {
+    return 1 if ($text =~ $regexp);
+  }
+
   #adverts, requests
   return 1 if ($text =~ /\@/ || $text =~ /^\s*\!/);
   return 1 if ($text =~ /slot\(s\)/);
+  return 1 if ($text =~ /~&~&~/);
 
   #xdcc
   return 1 if ($text =~ /^\#\d+/);
@@ -87,12 +150,20 @@ sub privmsg_handler {
   #}
   my @params = split / /, $_[0];
 
+  my $server = IRC::get_info(3);
+
   my $address = shift @params;
   my $constant = shift @params;
   my $chan= shift @params;
 
   #IRC::print("privmsg_handler: params=", @params, ":::\n");
   my $text = join ' ', @params;
+
+
+  return 0 unless $filter_turned_on;
+  if ($limit_to_server) {
+    return 0 unless $server == $limit_to_server;
+  }
 
   $text =~ s/^://;
 
@@ -101,7 +172,62 @@ sub privmsg_handler {
   #IRC::print("privmsg_handler: constant=$constant\n");
   #IRC::print("privmsg_handler: text=$text\n");
 
-  return (isFiltered($text));
+  return isFiltered($text);
+}
+
+sub filter_command_handler ( $ ) {
+  my ($arg) = $_[0];
+  my $server = IRC::get_info(3);
+
+  #IRC::print("/filter arg: |$arg|\n");
+  if ($arg =~ /^ON\b/i) {
+    $filter_turned_on = 1;
+    IRC::print("Filter turned ON\n");
+
+  } elsif ($arg =~ /^OFF\b/i) {
+    $filter_turned_on = 0;
+    IRC::print("Filter turned OFF\n");
+
+  } elsif ($arg =~ /^STATUS\b/i) {
+    if ($filter_turned_on) {
+      IRC::print("Filter is turned ON\n");
+    } else {
+      IRC::print("Filter is turned OFF\n");
+    }
+    if ($limit_to_server) {
+      IRC::print("Filtering limited to server $server\n");
+    }
+
+  } elsif ($arg =~ /^SERVER\b/i) {
+    if ($limit_to_server) {
+      IRC::print("Filter: Changing server from $limit_to_server to $server\n");
+    } else {
+      IRC::print("Filter: Limiting filtering to server $server\n");
+    }
+    $limit_to_server = $server;
+
+  } elsif ($arg =~ /^ALL\b/i) {
+    if ($limit_to_server) {
+      IRC::print("Filter: Removing limit to server $limit_to_server\n");
+    }
+    $limit_to_server = 0;
+
+  } elsif ($arg =~ /^HELP\b/i) {
+    IRC::print("/FILTER $command_list\n".
+	       "/FILTER SERVER - limits filtering to current server\n".
+	       "/FILTER ALL - resumes filtering everywhere\n".
+	       "/FILTER without parameter toggles filter\n");
+  } else {
+    IRC::print("/filter arg: |$arg|\n") if $arg;
+#     if ($filter_turned_on) {
+#       $filter_turned_on = 0;
+#       IRC::print("Filter turned OFF\n");
+#     } else {
+#       $filter_turned_on = 1;
+#       IRC::print("Filter turned ON\n");
+#     }
+  }
+  return 1;
 }
 
 1;
