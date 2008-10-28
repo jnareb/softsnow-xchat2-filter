c8ba94a7af47a03d20b438131235509bea233746
Jakub Narebski <jnareb@gmail.com>
2008-10-28 01:37:55 +0100

    Convert SoftSnow filter to use the new Perl interface for X-Chat 2
    Besides mechanical converting of IRC::* to Xchat::* it required to
    replace IRC::add_command_handler and IRC::add_message_handler by
    Xchat::hook_command and Xchat::hook_server, change code dealing with
    server and command callback arguments, different way of using
    Xchat::get_info (getting info by name rather than by number), and use
    Xchat::EAT_* constants
    
    Added description of the script, as Xchat::registor now supports it.
    
    Stripping of text decorations (bold, underline, etc.) is now done
    using built-in Xchat::strip_code() subroutine.
    
    Signed-off-by: Jakub Narebski <jnareb@gmail.com>

--- SoftSnow_filter.pl
+++ SoftSnow_filter.pl
@@ -7,14 +7,14 @@ use warnings;
 my $filter_file = "$ENV{'HOME'}/.xchat/SoftSnow_filter.conf";
 ### end config ###
 
-my $scriptName    = "SoftSnow XChat Filter";
-my $scriptVersion = "1.2.6";
+my $scriptName    = "SoftSnow XChat2 Filter";
+my $scriptVersion = "2.0";
+my $scriptDescr   = "Filter out file server announcements and IRC SPAM";
 
-IRC::register($scriptName, $scriptVersion, "", "");
+Xchat::register($scriptName, $scriptVersion, $scriptDescr);
 
-#IRC::add_command_handler("amsg", "amsg_command_handler");
-IRC::add_command_handler("filter",  "filter_command_handler");
-IRC::add_message_handler("PRIVMSG", "privmsg_handler");
+Xchat::hook_command("FILTER", \&filter_command_handler);
+Xchat::hook_server("PRIVMSG", \&privmsg_handler);
 
 my $B = chr 2;  # bold
 my $U = chr 31; # underline
@@ -22,8 +22,8 @@ my $C = chr 3;  # color
 
 my $command_list = 'ON|OFF|STATUS|SERVER|SERVERON|ALL|HELP|DEBUG|PRINT|ALLOW|ADD|DELETE|SAVE|LOAD';
 
-IRC::print("Loading ${B}$scriptName $scriptVersion${B}\n".
-           " For help: ${B}/FILTER HELP${B}\n");
+Xchat::print("Loading ${B}$scriptName $scriptVersion${B}\n".
+             " For help: ${B}/FILTER HELP${B}\n");
 
 my $filter_turned_on = 0; # was default turned ON
 my $limit_to_server  = 0; # don't limit to server (host)
@@ -31,15 +31,15 @@ my $use_filter_allow = 0; # use overrides
 
 # information about (default) options used
 if ($filter_turned_on) {
-	IRC::print("Filter turned ${B}ON${B}\n");
+	Xchat::print("Filter turned ${B}ON${B}\n");
 } else {
-	IRC::print("Filter turned ${B}OFF${B}\n");
+	Xchat::print("Filter turned ${B}OFF${B}\n");
 }
 if ($limit_to_server) {
-	IRC::print("Filter limited to server $limit_to_server\n")
+	Xchat::print("Filter limited to server $limit_to_server\n")
 }
 if ($use_filter_allow) {
-	IRC::print("Filter uses ALLOW rules\n")
+	Xchat::print("Filter uses ALLOW rules\n");
 }
 
 # ------------------------------------------------------------
@@ -88,14 +88,13 @@ my @filter_deny = (
 	q/brave soldier in the war/,
 );
 
+# return 1 (true) if text given as argument is to be filtered out
 sub isFiltered {
-	my $text = $_[0];
+	my $text = shift;
 	my $regexp = '';
 
-	#strip colour, underline, bold codes
-	$text =~ s/${B}//go; # code bold
-	$text =~ s/${U}//go; # code under
-	$text =~ s/${C}\d+(,\d+)?//go; # code colour
+	#strip colour, underline, bold codes, etc.
+	$text = Xchat::strip_code($text);
 
 	if ($use_filter_allow) {
 		foreach $regexp (@filter_allow) {
@@ -115,29 +114,24 @@ sub isFiltered {
 #2: PRIVMSG constant
 #3: channel
 #4: text said (prefixed with :)
-#return 0 to allow the text to be printed, 1 to filter it
 sub privmsg_handler {
-	$_[0] =~ s/\s{2,}/ /g;
-	my @params = split / /, $_[0];
+	# $_[0] - array reference containing the IRC message or command
+	#         and arguments broken into words
+	# $_[1] - array reference containing the Nth word to the last word
+	#my ($address, $constant, $chan) = @{$_[0]};
+	my $text = $_[1][3]; # Get server message
 
-	my $server = IRC::get_info(7); # host to be more exact; better for autoreconnect
-
-	my $address = shift @params;
-	my $constant = shift @params;
-	my $chan= shift @params;
-
-	#IRC::print("privmsg_handler: params=", @params, ":::\n");
-	my $text = join ' ', @params;
+	my $server = Xchat::get_info("host");
 
 
-	return 0 unless $filter_turned_on;
+	return Xchat::EAT_NONE unless $filter_turned_on;
 	if ($limit_to_server) {
-		return 0 unless $server eq $limit_to_server;
+		return Xchat::EAT_NONE unless $server eq $limit_to_server;
 	}
 
 	$text =~ s/^://;
 
-	return isFiltered($text);
+	return isFiltered($text) ? Xchat::EAT_XCHAT : Xchat::EAT_NONE;
 }
 
 # ------------------------------------------------------------
@@ -145,40 +139,40 @@ sub privmsg_handler {
 sub save_filter {
 	open F, ">$filter_file"
 		or do {
-			IRC::print("${B}FILTER:${B} Couldn't open file to save filter: $!\n");
+			Xchat::print("${B}FILTER:${B} Couldn't open file to save filter: $!\n");
 			return 1;
 		};
 
-	IRC::print("${B}FILTER SAVE >$filter_file${B}\n");
+	Xchat::print("${B}FILTER SAVE >$filter_file${B}\n");
 	foreach my $regexp (@filter_deny) {
-		IRC::print("/".$regexp."/ saved\n");
+		Xchat::print("/".$regexp."/ saved\n");
 		print F $regexp."\n";
 	}
-	IRC::print("${B}FILTER SAVED ----------${B}\n");
+	Xchat::print("${B}FILTER SAVED ----------${B}\n");
 	close F 
 		or do {
-			IRC::print("${B}FILTER:${B} Couldn't close file to save filter: $!\n");
+			Xchat::print("${B}FILTER:${B} Couldn't close file to save filter: $!\n");
 			return 1;
 		};
 	return 1;
 }
 
 sub load_filter {
-	IRC::print("${B}FILTER:${B} ...loading filter patterns\n");
+	Xchat::print("${B}FILTER:${B} ...loading filter patterns\n");
 	open F, "<$filter_file"
 		or do {
-			IRC::print("${B}FILTER:${B} Couldn't open file to load filter: $!\n");
+			Xchat::print("${B}FILTER:${B} Couldn't open file to load filter: $!\n");
 			return 1;
 		};
 	@filter_deny = <F>;
 	map (chomp, @filter_deny);
 	close F;
 
-	IRC::print("${B}FILTER DENY ----------${B}\n");
+	Xchat::print("${B}FILTER DENY ----------${B}\n");
 	for (my $i = 0; $i <= $#filter_deny; $i++) {
-		IRC::print(" [$i]: /".$filter_deny[$i]."/\n");
+		Xchat::print(" [$i]: /".$filter_deny[$i]."/\n");
 	}
-	IRC::print("${B}FILTER DENY ----------${B}\n");
+	Xchat::print("${B}FILTER DENY ----------${B}\n");
 }
 
 sub add_rule ( $ ) {
@@ -200,59 +194,59 @@ sub delete_rule ( $ ) {
 # ============================================================
 
 sub filter_command_handler ( $ ) {
-	my ($arg) = $_[0];
-	my $server = IRC::get_info(7);
+	my ($arg) = $_[1][1]; # 1st word to the last word
+	my $server = Xchat::get_info("host");
 
-	#IRC::print("/filter arg: |$arg|\n");
+	#Xchat::print("/filter arg: |$arg|\n");
 	if ($arg =~ /^ON\b/i) {
 		$filter_turned_on = 1;
-		IRC::print("Filter turned ON\n");
+		Xchat::print("Filter turned ON\n");
 
 	} elsif ($arg =~ /^OFF\b/i) {
 		$filter_turned_on = 0;
-		IRC::print("Filter turned OFF\n");
+		Xchat::print("Filter turned OFF\n");
 
 	} elsif ($arg =~ /^STATUS\b/i || !$arg) {
 		if ($filter_turned_on) {
-			IRC::print("Filter is turned ${B}ON${B}\n");
+			Xchat::print("Filter is turned ${B}ON${B}\n");
 		} else {
-			IRC::print("Filter is turned ${B}OFF${B}\n");
+			Xchat::print("Filter is turned ${B}OFF${B}\n");
 		}
 		if ($limit_to_server) {
-			IRC::print("Filter is limited to ".
-			           ($server eq $limit_to_server ? "${B}current${B} " : "" ).
-			           "server $limit_to_server");
+			Xchat::print("Filter is limited to ".
+			             ($server eq $limit_to_server ? "${B}current${B} " : "" ).
+			             "server $limit_to_server");
 		}
 		if ($use_filter_allow) {
-			IRC::print("Filter is using ALLOW rules (before DENY)\n");
+			Xchat::print("Filter is using ALLOW rules (before DENY)\n");
 		}
 
 	} elsif ($arg =~ /^SERVER\b/i) {
 		if ($limit_to_server) {
-			IRC::print("${B}FILTER:${B} Changing server from $limit_to_server to $server\n");
+			Xchat::print("${B}FILTER:${B} Changing server from $limit_to_server to $server\n");
 		} else {
-			IRC::print("${B}FILTER:${B} Limiting filtering to server $server\n");
+			Xchat::print("${B}FILTER:${B} Limiting filtering to server $server\n");
 		}
 		$limit_to_server = $server;
 
 	} elsif ($arg =~ /^SERVERON\b/i) {
 		if ($limit_to_server) {
-			IRC::print("${B}FILTER:${B} Changing server from $limit_to_server to $server\n");
+			Xchat::print("${B}FILTER:${B} Changing server from $limit_to_server to $server\n");
 		} else {
-			IRC::print("${B}FILTER:${B} Limiting filtering to server $server\n");
+			Xchat::print("${B}FILTER:${B} Limiting filtering to server $server\n");
 		}
 		$limit_to_server = $server;
 
 		$filter_turned_on = 1;
-		IRC::print("Filter turned ${B}ON${B}\n");
+		Xchat::print("Filter turned ${B}ON${B}\n");
 	} elsif ($arg =~ /^ALL\b/i) {
 		if ($limit_to_server) {
-			IRC::print("Filter: Removing limit to server $limit_to_server\n");
+			Xchat::print("Filter: Removing limit to server $limit_to_server\n");
 		}
 		$limit_to_server = 0;
 
 	} elsif ($arg =~ /^HELP\b/i) {
-		IRC::print(<<"EOF");
+		Xchat::print(<<"EOF");
 ${B}/FILTER $command_list${B}
 /FILTER ON|OFF - turns filtering on/off
 /FILTER HELP - prints this help message
@@ -272,74 +266,74 @@ ${B}/FILTER $command_list${B}
 EOF
 
 	} elsif ($arg =~ /^VERSION\b/i) {
-		IRC::print("${B}$scriptName $scriptVersion${B}\n");
+		Xchat::print("${B}$scriptName $scriptVersion${B}\n");
 
 	} elsif ($arg =~ /^DEBUG\b/i || $arg =~ /^INFO\b/i) {
-		IRC::print("${B}FILTER DEBUG ----------${B}\n");
-		IRC::print("Channel: ".IRC::get_info(2)."\n");
-		IRC::print("Server:  ".IRC::get_info(3)."\n");
-		IRC::print("Network: ".IRC::get_info(6)."\n");
-		IRC::print("Host:    ".IRC::get_info(7)."\n");
-		IRC::print("${B}FILTER DEBUG ----------${B}\n");
+		Xchat::print("${B}FILTER DEBUG ----------${B}\n");
+		Xchat::print("Channel: ".Xchat::get_info("channel")."\n");
+		Xchat::print("Server:  ".Xchat::get_info("server")."\n");
+		Xchat::print("Network: ".Xchat::get_info("network")."\n");
+		Xchat::print("Host:    ".Xchat::get_info("host")."\n");
+		Xchat::print("${B}FILTER DEBUG ----------${B}\n");
 
 	} elsif ($arg =~ /^PRINT/i) {
-		IRC::print("${B}FILTER PRINT ----------${B}\n");
-		IRC::print("${B}ALLOW${B}".($use_filter_allow ? ' (on)' : ' (off)')."\n");
+		Xchat::print("${B}FILTER PRINT ----------${B}\n");
+		Xchat::print("${B}ALLOW${B}".($use_filter_allow ? ' (on)' : ' (off)')."\n");
 		for (my $i = 0; $i <= $#filter_allow; $i++) {
-			IRC::print("[$i]: /".$filter_allow[$i]."/\n");
+			Xchat::print("[$i]: /".$filter_allow[$i]."/\n");
 		}
-		IRC::print("${B}DENY${B}\n");
+		Xchat::print("${B}DENY${B}\n");
 		for (my $i = 0; $i <= $#filter_deny; $i++) {
-			IRC::print("[$i]: /".$filter_deny[$i]."/\n");
+			Xchat::print("[$i]: /".$filter_deny[$i]."/\n");
 		}
-		IRC::print("${B}FILTER PRINT ----------${B}\n");
+		Xchat::print("${B}FILTER PRINT ----------${B}\n");
 
 	} elsif ($arg =~ /^ALLOW/i) {
 		$use_filter_allow = !$use_filter_allow;
-		IRC::print("${B}FILTER:${B} ALLOW rules ".
-		           ($use_filter_allow ? "enabled" : "disabled")."\n");
+		Xchat::print("${B}FILTER:${B} ALLOW rules ".
+		             ($use_filter_allow ? "enabled" : "disabled")."\n");
 
 	} elsif ($arg =~ /^ADD\s+(.*)/i) {
 		if ($1) {
 			add_rule($1);
-			IRC::print("${B}FILTER RULE [$#filter_deny]:${B} /$1/\n");
+			Xchat::print("${B}FILTER RULE [$#filter_deny]:${B} /$1/\n");
 		} else {
-			IRC::print("Syntax: ${B}/FILTER ADD ${U}rule${U}${B} to add\n")
+			Xchat::print("Syntax: ${B}/FILTER ADD ${U}rule${U}${B} to add\n")
 		}
 
 	} elsif ($arg =~ /^DELETE(.*)/i) {
 		my $num = $1;
-		$num =~ s/^\s*(.*?)\s*$/\1/g;
+		$num =~ s/^\s*(.*?)\s*$/$1/g;
 	SWITCH: {
 			unless ($num) {
-				IRC::print("${B}FILTER:${B} deleting /".$filter_deny[-1]."/\n");
+				Xchat::print("${B}FILTER:${B} deleting /".$filter_deny[-1]."/\n");
 				$#filter_deny--;
-				IRC::print("${B}FILTER:${B} deleted successfully last rule\n");
+				Xchat::print("${B}FILTER:${B} deleted successfully last rule\n");
 				last SWITCH;
 			}
 			if ($num !~ /^\d+$/) { 
-				IRC::print("${B}FILTER:${B} $num is not a number\n");
+				Xchat::print("${B}FILTER:${B} $num is not a number\n");
 				last SWITCH;
 			}
 			if ($num < 0 || $num > $#filter_deny) {
-				IRC::print("${B}FILTER:${B} $num outside range [0,$#filter_deny]\n");
+				Xchat::print("${B}FILTER:${B} $num outside range [0,$#filter_deny]\n");
 				last SWITCH;
 			}
-			IRC::print("${B}FILTER:${B} deleting /".$filter_deny[$num]."/\n");
+			Xchat::print("${B}FILTER:${B} deleting /".$filter_deny[$num]."/\n");
 			delete_rule($num);
-			IRC::print("${B}FILTER:${B} deleted successfully rule $num\n");
+			Xchat::print("${B}FILTER:${B} deleted successfully rule $num\n");
 		}
 
 	} elsif ($arg =~ /^SAVE/i) {
 		save_filter();
-		IRC::print("${B}FILTER:${B} saved DENY rules to $filter_file\n");
+		Xchat::print("${B}FILTER:${B} saved DENY rules to $filter_file\n");
 
 	} elsif ($arg =~ /^(RE)?LOAD/i) {
 		load_filter();
-		IRC::print("${B}FILTER:${B} loaded DENY rules from $filter_file\n");
+		Xchat::print("${B}FILTER:${B} loaded DENY rules from $filter_file\n");
 
 	} else {
-		IRC::print("/filter arg: |$arg|\n") if $arg;
+		Xchat::print("/filter arg: |$arg|\n") if $arg;
 	}
 	return 1;
 }
